5c6a3446e82dd9d32dfb0fbef453fb2b58b58828
Neroal <b0543019@ems.niu.edu.tw>
2020-03-14 20:48:52 +0800

    5

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -19,3 +19,17 @@ window.onload = function() {
 		$picker.style['display'] = 'none';	
 	}
 }
+
+window.onscroll = function()  { 
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}//網頁捲動超過200pixel就會跑出來 display設定成block 跑回上面就隱藏。
+ 
+// 重置scrollTop這個變數的值
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
