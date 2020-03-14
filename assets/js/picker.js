8b61fe597fafa98f033fdd9b989e4e0af148fa79
Neroal <b0543019@ems.niu.edu.tw>
2020-03-14 21:21:42 +0800

    123

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -20,9 +20,3 @@ window.onload = function() {
 	}
 }
 
-
-// 重置scrollTop這個變數的值
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
