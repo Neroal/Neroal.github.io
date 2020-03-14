28c461a4e637a38317a493465969e50135cb1b83
Neroal <b0543019@ems.niu.edu.tw>
2020-03-14 21:32:05 +0800

    2

--- assets/js/picker.js
+++ assets/js/picker.js
@@ -20,3 +20,7 @@ window.onload = function() {
 	}
 }
 
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
